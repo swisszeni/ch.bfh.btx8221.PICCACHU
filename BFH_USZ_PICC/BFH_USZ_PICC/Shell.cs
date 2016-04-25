8597e6eca2b379522239a1c1c63ce1fee021ea24
Raphael Zenhäusern <raphael@zenhausern.net>
2016-04-25 18:09:07 +0200

    Establishing base navigation layout. Pt 2. WIP ->compilation will fail.

--- BFH_USZ_PICC/BFH_USZ_PICC/Shell.cs
+++ BFH_USZ_PICC/BFH_USZ_PICC/Shell.cs
@@ -1,4 +1,6 @@
 ﻿using BFH_USZ_PICC.Models;
+using BFH_USZ_PICC.ViewModels;
+using BFH_USZ_PICC.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +16,65 @@ namespace BFH_USZ_PICC
 
         public Shell()
         {
+            Pages = new Dictionary<MenuItemKey, NavigationPage>();
+            Master = new MenuPage(this);
+            BindingContext = new BaseViewModel
+            {
+                Title = "Hanselman",
+                Icon = "slideout.png"
+            };
+            //setup home page
+            NavigateAsync(MenuItemKey.PICC);
 
+            InvalidateMeasure();
+        }
+
+        public async Task NavigateAsync(MenuItemKey id)
+        {
+            Page newPage;
+            if (!Pages.ContainsKey(id))
+            {
+
+                switch (id)
+                {
+                    case MenuType.About:
+                        Pages.Add(id, new HanselmanNavigationPage(new AboutPage()));
+                        break;
+                    case MenuType.Blog:
+                        Pages.Add(id, new HanselmanNavigationPage(new BlogPage()));
+                        break;
+                    case MenuType.DeveloperLife:
+                        Pages.Add(id, new HanselmanNavigationPage(new PodcastPage(id)));
+                        break;
+                    case MenuType.Hanselminutes:
+                        Pages.Add(id, new HanselmanNavigationPage(new PodcastPage(id)));
+                        break;
+                    case MenuType.Ratchet:
+                        Pages.Add(id, new HanselmanNavigationPage(new PodcastPage(id)));
+                        break;
+                    case MenuType.Twitter:
+                        Pages.Add(id, new HanselmanNavigationPage(new TwitterPage()));
+                        break;
+                }
+            }
+
+            newPage = Pages[id];
+            if (newPage == null)
+                return;
+
+            //pop to root for Windows Phone
+            if (Detail != null && Device.OS == TargetPlatform.WinPhone)
+            {
+                await Detail.Navigation.PopToRootAsync();
+            }
+
+            Detail = newPage;
+
+            if (IsUWPDesktop)
+                return;
+
+            if (Device.Idiom != TargetIdiom.Tablet)
+                IsPresented = false;
         }
 
     }
---
+++ BFH_USZ_PICC/BFH_USZ_PICC/Views/BaseView.cs
@@ -0,0 +1,14 @@
+﻿using BFH_USZ_PICC.ViewModels;
+using Xamarin.Forms;
+
+namespace BFH_USZ_PICC.Views
+{
+    public class BaseView : ContentPage
+    {
+        public BaseView()
+        {
+            SetBinding(Page.TitleProperty, new Binding(BaseViewModel.TitlePropertyName));
+            SetBinding(Page.IconProperty, new Binding(BaseViewModel.IconPropertyName));
+        }
+    }
+}
