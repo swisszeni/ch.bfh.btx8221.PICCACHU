b30443368d0bf0d0471ef1d5d68a0960ab28d335
Raphael Zenhäusern <raphael@zenhausern.net>
2016-04-27 17:10:06 +0200

    Improved displayed Naming and added support vor API21+

--- BFH_USZ_PICC/BFH_USZ_PICC.Droid/MainActivity.cs
+++ BFH_USZ_PICC/BFH_USZ_PICC.Droid/MainActivity.cs
@@ -6,10 +6,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Graphics.Drawables;
 
 namespace BFH_USZ_PICC.Droid
 {
-    [Activity(Label = "BFH_USZ_PICC", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "USZ PICC", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -18,6 +19,12 @@ namespace BFH_USZ_PICC.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+            if ((int)Android.OS.Build.VERSION.SdkInt >= 21)
+            {
+                ActionBar.SetIcon(
+                  new ColorDrawable(Resources.GetColor(Android.Resource.Color.Transparent)));
+            }
         }
     }
 }
