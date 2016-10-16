c64ae6d52c82f03b5e09cf4650cb7afb8d35e443
Raphael Zenhäusern <raphael@zenhausern.net>
2016-10-16 19:41:01 +0200

    Completed implementation of i18n infrastructure

---
+++ BFH_USZ_PICC/BFH_USZ_PICC/Utilitys/Extensions/TranslateExtension.cs
@@ -0,0 +1,51 @@
+﻿using BFH_USZ_PICC.Interfaces;
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BFH_USZ_PICC.Utilitys.Extensions
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci;
+        const string ResourceId = "UsingResxLocalization.Resx.AppResources";
+
+        public TranslateExtension()
+        {
+            if (Device.OS == TargetPlatform.iOS || Device.OS == TargetPlatform.Android)
+            {
+                ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            }
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
+
+            ResourceManager resmgr = new ResourceManager(ResourceId
+                                , typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            var translation = resmgr.GetString(Text, ci);
+
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    String.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
+                    "Text");
+#else
+                // HACK: returns the key, which gets displayed to the user
+                translation = Text; 
+#endif
+            }
+            return translation;
+        }
+    }
+}
