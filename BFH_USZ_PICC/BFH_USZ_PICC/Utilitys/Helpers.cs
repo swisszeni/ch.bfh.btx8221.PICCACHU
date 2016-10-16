b292d8176f6fd2a12dcd61155e0afcfe44069328
Raphael Zenhäusern <raphael@zenhausern.net>
2016-10-16 18:59:48 +0200

    Added multilingual resources

---
+++ BFH_USZ_PICC/BFH_USZ_PICC/Utilitys/Helpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFH_USZ_PICC.Utilitys
+{
+    /// <summary>
+    /// Helper class to transform platform culture strings
+    /// </summary>
+    public class PlatformCulture
+    {
+        public PlatformCulture(string platformCultureString)
+        {
+            if (String.IsNullOrEmpty(platformCultureString))
+            {
+                throw new ArgumentException("Expected culture identifier", nameof(platformCultureString));
+            }
+            // .NET expects dash, not underscore
+            PlatformString = platformCultureString.Replace("_", "-");
+            var dashIndex = PlatformString.IndexOf("-", StringComparison.Ordinal);
+            if (dashIndex > 0)
+            {
+                var parts = PlatformString.Split('-');
+                LanguageCode = parts[0];
+                LocaleCode = parts[1];
+            }
+            else
+            {
+                LanguageCode = PlatformString;
+                LocaleCode = "";
+            }
+        }
+        public string PlatformString { get; private set; }
+        public string LanguageCode { get; private set; }
+        public string LocaleCode { get; private set; }
+        public override string ToString()
+        {
+            return PlatformString;
+        }
+    }
+}
