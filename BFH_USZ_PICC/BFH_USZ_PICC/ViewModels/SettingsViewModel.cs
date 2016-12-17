b4738e2593936fdd00bcc94bc5e5a955b0734084
Florian Schnyder <schnf8@bfh.ch>
2016-12-17 10:43:22 +0100

    Transfered SettingsPage to MVVM Light

---
+++ BFH_USZ_PICC/BFH_USZ_PICC/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,128 @@
+﻿using BFH_USZ_PICC.Interfaces;
+using BFH_USZ_PICC.Models;
+using BFH_USZ_PICC.Resx;
+using BFH_USZ_PICC.Views;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BFH_USZ_PICC.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        private IReminderNotification _notifier = DependencyService.Get<IReminderNotification>();
+       
+        private DateTimeOffset _reminderStartDate = DateTimeOffset.Now;
+        public DateTimeOffset ReminderStartDate
+        {
+            get { return _reminderStartDate; }
+            set { Set(ref _reminderStartDate, value); }
+        }
+
+        private TimeSpan _reminderDayTime;
+        public TimeSpan ReminderDayTime
+        {
+            get { return _reminderDayTime; }
+            set { Set(ref _reminderDayTime, value); }
+        }
+
+        private int _reminderFrequency;
+        public int ReminderFrequency
+        {
+            get { return _reminderFrequency; }
+            set { Set(ref _reminderFrequency, value); }
+        }
+
+        private int _reminderRepetition;
+        public int ReminderRepetition
+        {
+            get { return _reminderRepetition; }
+            set { Set(ref _reminderRepetition, value); }
+        }
+
+        private bool _isReminderSet;
+        public bool IsReminderSet
+        {
+            get { return _isReminderSet; }
+            set
+            {
+                Set(ref _isReminderSet, value);
+                CheckReminderToggleCommand.Execute(null);
+            }
+
+        }
+
+        private bool _isReminderEditable = true;
+        public bool IsReminderEditable
+        {
+            get { return _isReminderEditable; }
+            set { Set(ref _isReminderEditable, value); }
+
+        }
+
+
+
+        private RelayCommand _addNotificationsCommand;
+        public RelayCommand AddNotificationsCommand => _addNotificationsCommand ?? (_addNotificationsCommand = new RelayCommand(() =>
+        {
+            IsReminderEditable = false;
+
+            DateTimeOffset reminderStartDate = ReminderStartDate.Date;
+            DateTimeOffset maintenanceReminderStartDateTime = reminderStartDate.Add(ReminderDayTime);
+            maintenanceReminderStartDateTime.ToUniversalTime();
+
+            _notifier.AddNotification(maintenanceReminderStartDateTime, ReminderFrequency + 1, ReminderRepetition, AppResources.InformationText, AppResources.SettingsPageMaintenanceReminderInformationText);
+
+            //var notification = new LocalNotification
+            //{
+            //    Text = "Hello from Plugin",
+            //    Title = "Notification Plugin",
+            //    Id = 1,
+            //    NotifyTime = DateTime.Now
+            //};
+
+            //var notifier = CrossLocalNotifications.CreateLocalNotifier();
+            //notifier.Notify(notification);
+
+
+
+
+        }));
+
+
+        private RelayCommand _moveToMasterDataPageCommand;
+        public RelayCommand MoveToMasterDataPageCommand => _moveToMasterDataPageCommand ?? (_moveToMasterDataPageCommand = new RelayCommand(() =>
+        {
+            ((Shell)Application.Current.MainPage).Detail.Navigation.PushAsync(new BasePage(typeof(UserMasterDataPage), new List<object> { false }));
+
+        }));
+
+        private RelayCommand _checkReminderToggleCommand;
+        public RelayCommand CheckReminderToggleCommand => _checkReminderToggleCommand ?? (_checkReminderToggleCommand = new RelayCommand(async () =>
+        {
+            if (!IsReminderEditable && !IsReminderSet)
+            {
+                if (await Application.Current.MainPage.DisplayAlert(AppResources.WarningText, AppResources.SettingsPageDelteScheduledRemindersText, AppResources.YesButtonText, AppResources.NoButtonText))
+                {
+                    IsReminderEditable = true;
+                    //notifier.RemoveAllNotifications();
+                }
+                else
+                {
+                    IsReminderSet = true;
+                }
+
+
+            }
+
+        }));
+
+
+    }
+}
--- BFH_USZ_PICC/BFH_USZ_PICC/Views/SettingsPage.xaml.cs
+++ BFH_USZ_PICC/BFH_USZ_PICC/Views/SettingsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BFH_USZ_PICC.Interfaces;
 using BFH_USZ_PICC.Resx;
+using BFH_USZ_PICC.ViewModels;
 using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
@@ -9,124 +10,24 @@ namespace BFH_USZ_PICC.Views
 {
 
     public sealed partial class SettingsPage : BaseContentPage
-    {
-        IReminderNotification notifier; 
-        private List<string> plannedNotifications;
-
+    {       
         public SettingsPage(ContentPage contained) : base(contained)
         {
             InitializeComponent();
 
-            notifier = DependencyService.Get<IReminderNotification>();
-            plannedNotifications = new List<string>();
-
-            int temp = 0;
-            while (temp < 20)
+            int temp = 1;
+            Repetition.Items.Insert(0, "Unbgerenzt");
+            while (temp < 50)
             {
                 Repetition.Items.Insert(temp, temp.ToString());
                 //Frequency needs to start at 1!
-                Frequency.Items.Insert(temp, (temp + 1).ToString());
+                Frequency.Items.Insert(temp - 1, temp.ToString());
                 temp++;
             }
             Repetition.SelectedIndex = 0;
             Frequency.SelectedIndex = 6;
 
         }
-
-        void AddNotificationButtonClicked(object o, EventArgs e)
-        {
-            EditWeeklyReminder.IsEnabled = false;
-            EnableInputForNotifications(false);
-
-            DateTimeOffset reminderStartDate = ReminderStartDate.Date;
-            DateTimeOffset maintenanceReminderStartDateTime = reminderStartDate.Add(ReminderDailyTime.Time);
-            maintenanceReminderStartDateTime.ToUniversalTime();
-            int maintenanceReminderRepetition = Repetition.SelectedIndex;
-            int dailyInterval = Frequency.SelectedIndex + 1;
-
-            notifier.AddNotification(maintenanceReminderStartDateTime, dailyInterval, maintenanceReminderRepetition, AppResources.InformationText, AppResources.SettingsPageMaintenanceReminderInformationText);
-
-        }
-
-        void PersonalMasterDataClicked(object o, EventArgs e)
-        {
-            Navigation.PushAsync(new BasePage(typeof(UserMasterDataPage), new List<object> { false }));
-        }
-
-        async void WeeklyMaintenanceReminderToggled(object o, EventArgs e)
-        {
-            if (WeeklyMaintenanceReminder.IsToggled)
-            {
-                EditWeeklyReminder.IsVisible = true;
-                
-            }
-            else
-            {
-                bool deleteReminders = await DisplayAlert(AppResources.WarningText , AppResources.SettingsPageDelteScheduledRemindersText, AppResources.YesButtonText, AppResources.NoButtonText);
-                if (deleteReminders)
-                {
-                    EditWeeklyReminder.IsVisible = false;
-                    EnableInputForNotifications(true);
-                    notifier.RemoveAllNotifications();                    
-                }
-                else
-                {
-                    WeeklyMaintenanceReminder.IsToggled = true;
-                }
-            }
-        }
-
-        private void EnableInputForNotifications(bool yesOrNo)
-        {
-            AddNotificationButton.IsEnabled = yesOrNo;
-            ReminderStartDate.IsEnabled = yesOrNo;
-            ReminderDailyTime.IsEnabled = yesOrNo;
-            Repetition.IsEnabled = yesOrNo;
-            Frequency.IsEnabled = yesOrNo;
-        }
+        
     }
 }
-
-//    try
-//    {
-//        Notifications.Instance.CancelAll();
-
-//    }
-//    catch
-//    {
-//        foreach (var noti in plannedNotifications)
-//        {
-//            Notifications.Instance.Cancel(noti);
-//        }
-//    }
-
-//    EditWeeklyReminder.IsVisible = false;
-//    enableInputForNotifications(true);
-
-// int reminderRepetition = 0;
-//TimeSpan firstReminder = maintenanceReminderStartDateTime - DateTimeOffset.UtcNow;
-//int reminderFrequencyinDays = 7;
-//Notification not = new Notification();
-//not.Title = "Wartung fällig!";
-//not.Message = "Ihr PICC Katheter sollte gewartet werden.";            
-
-////this loop checks how many reminder repetation the user wants to plan
-//while (reminderRepetition <= Repetition.SelectedIndex)
-//{
-//    //Checks if the notification date time is in the future. If not, the notification can not be planned.
-//   // not.When = firstReminder + (new TimeSpan(0, 0, 0, reminderRepetition * reminderFrequencyinDays));
-//    not.SetSchedule(firstReminder + (new TimeSpan(0, 0, 0, reminderRepetition * reminderFrequencyinDays)));
-
-//    try
-//    {
-//        var notificationId = Notifications.Instance.Send(not);
-//        plannedNotifications.Add(notificationId);
-//    }
-//    catch
-//    {
-//       //not.SetSchedule(not.When);
-//       Notifications.Instance.Send(not);
-//    }
-
-//    reminderRepetition++;
-//}
