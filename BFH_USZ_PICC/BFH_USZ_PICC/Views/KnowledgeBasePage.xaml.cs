7c3b83adeb45e6f565fc0bbac742c96cd1ca43ff
Raphael Zenhäusern <raphael@zenhausern.net>
2016-12-19 00:06:41 +0100

    Started Implementation

---
+++ BFH_USZ_PICC/BFH_USZ_PICC/Views/KnowledgeBasePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BFH_USZ_PICC.Views
+{
+    public sealed partial class KnowledgeBasePage : BaseContentPage
+    {
+        public KnowledgeBasePage(ContentPage contained) : base(contained)
+        {
+            InitializeComponent();
+        }
+    }
+}
