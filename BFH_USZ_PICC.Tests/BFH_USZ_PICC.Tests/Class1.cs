c9fedb6ed42c6c718fa960ebcc303aebe2c4bcde
Raphael Zenhäusern <raphael@zenhausern.net>
2017-01-09 01:38:36 +0100

    Implemented UnitTest project structure (#23)
    * Added testing foundation for xUnit test to solution
    
    * Fixed UnitTest references
    
    * Added UWP TestRunner

---
+++ BFH_USZ_PICC.Tests/BFH_USZ_PICC.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BFH_USZ_PICC.Tests
+{
+    public class TestsAreGood
+    {
+        [Fact]
+        public void ThisShouldPass()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task ThisShouldFail()
+        {
+            await Task.Run(() => { throw new Exception("boom"); });
+        }
+    }
+}
